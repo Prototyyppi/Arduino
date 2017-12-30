c80909295acc0cc41f0d9b45e85597cb0a7bd30d
Unknown <jani.laitinen@gmx.com>
2017-12-30 13:57:58 +0200

    Added 3 led dimming and readme
    Draft developement

--- arduino_led_fader/arduino_led_fader.ino
+++ arduino_led_fader/arduino_led_fader.ino
@@ -17,24 +17,56 @@ pinMode(blue_led, OUTPUT);
 
 void loop()
 {
-int pin;
-for (pin = 0; pin<=2; pin++) {
-  dimming_function(pin_array[pin]);
-}
-pin = 0;
-
+dimming_function(pin_array);
 }
 
-void dimming_function(const int led_pin)
+void dimming_function(const int led_pin[])
 {
 
-int brightness;
+int brightness_1 = 0, brightness_2 = 0, brightness_3 = 0;
 int fade_direction = 1;
-for(brightness = 1; (brightness <= 255 || brightness = 0); brightness + fade_direction) {
-  analogWrite(led_pin, brightness);
-  if (brightness = 255) {
+int brightness_2_trigd = 0;
+int brightness_3_trigd = 0;
+int speed_randint_1, speed_randint_2;
+while (true){
+speed_randint_1 = random(1, 3);
+speed_randint_2 = random(1, 3);
+for (brightness_1 = 1; (brightness_1 <= 255 || brightness_1 = 0); brightness_1 = brightness_1 + fade_direction) {
+  analogWrite(led_pin[0], brightness_1);
+  analogWrite(led_pin[1], brightness_2);
+  analogWrite(led_pin[2], brightness_3);
+  
+  if (brightness_1 = 255)
     fade_direction = -1;
+
+/* For led 2 */
+  if (brightness_2_trigd == 1) {
+    brightness_2 += speed_randint_1 * -1;
+    if (brightness_2 < 5) {
+    brightness_2_trigd == 0;
+    brightness_2 += speed_randint_1 * 1;
+    }
   }
+  else {
+    brightness_2 += speed_randint_1 * 1;
+    if (brightness2 > 250)
+      brightness_2_trigd == 1;
+  }
+
+  /* For led 3 */
+  if (brightness_3_trigd == 1) {
+    brightness_3 += speed_randint_2 * -1;
+    if (brightness_2 < 5) {
+    brightness_3_trigd == 0;
+    brightness_3 += speed_randint_2 * 1;
+    }
+  }
+  else {
+    brightness_3 += speed_randint_2 * 1;
+    if (brightness3 > 250)
+      brightness_3_trigd == 1;
+  }
+}
 }
 }
 
